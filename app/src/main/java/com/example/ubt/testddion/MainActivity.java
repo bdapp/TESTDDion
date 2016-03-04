5fc185c2f5771974942ffbc31ad310bd8d181de6
belloving= <belloving@gmail.com>
2016-03-04 17:53:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/ubt/testddion/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.ubt.testddion;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        holdTest();
+    }
+
+    private void holdTest() {
+        Log.i("hell", "[[[[[[[[[[[[[[[");
+
+
+    }
+}
